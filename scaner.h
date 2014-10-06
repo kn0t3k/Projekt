8498e8090c6129c570fa16878b1aa9c9018e15dd
kn0t3k <martinkntk@gmail.com>
2014-10-06 21:24:11 +0200

    update

--- scaner.h
+++ scaner.h
@@ -8,25 +8,41 @@
 /*
 zmenit !!!
 */
-#define SETZERO 10
-#define READ    11
-#define WRITE   12
-#define WHILE   13
+#define BEGIN 10
+#define BOOLEAN    11
+#define DO   12
+#define ELSE   13
+#define END   14
+#define FALSE   15
+#define FIND   16
+#define FORWARD   17
+#define FUNCTION   18
+#define IF   19
+#define INTEGER 20
+#define READLN 21
+#define REAL 22
+#define SORT 23
+#define STRING 24
+#define THEN 25
+#define TRUE 26
+#define VAR 27
+#define WHILE 28
+#define WRITE 29
 
 //operatory inkrementace a dekrementace
-#define INC     20
-#define DEC     21
+#define INC     30
+#define DEC     31
 
 //jednotlive znaky
-#define LEFT_VINCULUM  30 // '{'
-#define RIGHT_VINCULUM 31 // '}'
-#define SEMICOLON      32 // ';'
+#define LEFT_VINCULUM  40 // '{'
+#define RIGHT_VINCULUM 41 // '}'
+#define SEMICOLON      42 // ';'
 
 //specialni znaky
-#define END_OF_FILE    40
+#define END_OF_FILE    52
 
 //chybove hlasky
-#define LEX_ERROR    -1
+#define LEX_ERROR    1
 
 //hlavicka funkce simulujici lexikalni analyzator
 void setSourceFile(FILE *f);
