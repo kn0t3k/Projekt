f690f1466254c388ea6a0cea441c141575e6ac50
kn0t3k <martinkntk@gmail.com>
2014-10-06 18:08:33 +0200

    projekt
    zdrojaky

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "str.h"
+#include "stable.h"
+#include "ilist.h"
+#include "scaner.h"
+#include "parser.h"
+#include "interpret.h"
+
+#define INTERNAL_ERR 99
+
+int main(int argc, char** argv)
+{
+   FILE *f;
+   if (argc == 1)
+   {
+      fprintf(stderr, "Neni zadan vstupni soubor\n");
+      return INTERNAL_ERR;
+   }
+   if ((f = fopen(argv[1], "r")) == NULL)
+   {
+      fprintf(stderr, "Soubor se nepodarilo otevrit\n");
+      return INTERNAL_ERR;
+   }
+
+   setSourceFile(f);
+
+   tSymbolTable ST;
+   tableInit(&ST); // inicializace tabulky symbolu
+
+   tListOfInstr instrList;
+   listInit(&instrList); // inicializace seznamu instrukci
+
+   int result;
+   result = parse(&ST, &instrList); // provedeme syntaktickou analyzu
+
+   switch (result)
+   {
+     case LEX_ERROR:
+     case SYNTAX_ERROR:
+     case SEM_ERROR:
+       // nastala chyba v prubehu prekladu
+       tableFree(&ST);
+       listFree(&instrList);
+       fclose(f);
+       return -result;
+     break;
+     // jinak probehlo vse v poradku, muzeme provadet kod
+   }
+
+   // provedeme interpretaci
+   inter(&ST, &instrList);
+
+   tableFree(&ST);
+   listFree(&instrList);
+   fclose(f);
+   return 0;
+}
