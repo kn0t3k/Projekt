c0e102bf45f3d07cf8d98dd4cd039ad082df0ca5
Jan Kohút <kohut.honza@seznam.cz>
2014-11-22 20:51:45 +0100

    mega update

--- parser.c
+++ parser.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "str.h"
 #include "stable.h"
 #include "ilist.h"
@@ -7,10 +8,10 @@
 
 int token;/*Globalni promena*/
 
-int parse()
-{
-  printf("ahoj_parse\n");
+int parse(){
+  
   int result;
+  
   strInit(&attr);
   if ((token = getNextToken(&attr)) == LEX_ERROR)
      result = LEX_ERROR;
@@ -22,43 +23,49 @@ int parse()
 
 int program(){ /*<PROGRAM>*/
   
-  token = getNextToken();
+  int result;
   
   switch (token){
     /*<PROGRAM> -> <DECLARATION> <FUNCTION> <BODY> DOT*/
     case VAR:
     case FUNCTION:
     case BEGIN:
-      
-	  if ((declaration()) != SYNTAX_OK) return SYNTAX_ERROR;/*<DECLARATION>*/  
-	  if ((function()) != SYNTAX_OK) return SYNTAX_ERROR;/*<FUNCTION>*/	  
-	  if ((body()) != SYNTAX_OK) return SYNTAX_ERROR;/*BODY*/  
-	  if (token != DOT)/*DOT*/ 
-	    return SYNTAX_ERROR;
-	  else	  
-	    if ((getNextToken()) != END_OF_FILE) return SYNTAX_ERROR;
-	  
-	  return SYNTAX_OK;/*Pokud zadne z pravidel nevrati SYNTAX_ERROR*/
-	  
+      result = declaration();
+	  if (result != SYNTAX_OK) return result;/*<DECLARATION>*/
+      result = function();	  
+	  if (result != SYNTAX_OK) return result;/*<FUNCTION>*/
+      result = body();	  
+	  if (result != SYNTAX_OK) return result;/*BODY*/  
+	  if (token != DOT) return SYNTAX_ERROR;/*DOT*/
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  if (token != END_OF_FILE) return SYNTAX_ERROR;
+	  return SYNTAX_OK;/*Pokud zadne z pravidel nevrati SYNTAX_ERROR, LEX_ERROR nebo SEM_ERROR*/
 	  break;
 	
 	default:/*Pokud je prvni token jiny nez povoleny*/
       return SYNTAX_ERROR;	
+	  break;
 	}	
 }
 
 int declaration(){/*<DECLARATION>*/
 
+  int result;
+
   switch (token){
     /*<DECLARATION> -> VAR ID COLON <TYPE> SEMICOLON <N_DECLARATION>*/
 	case VAR:
-	  if ((getNextToken()) != ID) return SYNTAX_ERROR;
-	  if ((getNextToken()) != COLON) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  if (token != ID) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  if (token != COLON) return SYNTAX_ERROR;
 	  token = getNextToken();
-      if ((type()) != SYNTAX_OK) return SYNTAX_ERROR;
+	  result = type();
+      if (result != SYNTAX_OK) return result;
 	  if (token != SEMICOLON) return SYNTAX_ERROR;
-	  token = getNextToken();
-	  if ((n_declaration()) != SYNTAX_OK) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  result = n_declaration();
+	  if (result != SYNTAX_OK) return result;
       return SYNTAX_OK;	      
 	  break;
 	
@@ -70,36 +77,43 @@ int declaration(){/*<DECLARATION>*/
 	  
 	default:/*Nemuze nastat, ale z konvence to pridavam*/
 	  return SYNTAX_ERROR;
+	  break;
   }
 }
 
-int type (){/*<TYPE>*/
+int type(){/*<TYPE>*/
 
   switch (token){
     /*<TYPE> -> T_INTEGER, pro ostatni case analogicky*/  
     case T_INTEGER:
 	case T_REAL:
 	case T_STRING:
-	  token = getNextToken();/*Pokud dane pravidlo pokryje nejaky token, vola dalsi*/
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;/*Pokud dane pravidlo pokryje nejaky token, vola dalsi*/
 	  return SYNTAX_OK;
 	  break;
 	  
 	default:
       return SYNTAX_ERROR;
+	  break;
   }	  
 }
 
 int n_declaration(){/*<N_DECLARATION>*/
   
+  int result;
+  
   switch (token){
     /*<N_DECLARATION> -> ID COLON <TYPE> SEMICOLON <N_DECLARATION>*/
     case ID:
-	  if ((getNextToken()) != COLON) return SYNTAX_ERROR;
-	  token = getNextToken();
-	  if ((type()) != SYNTAX_OK) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  if (token != COLON) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  result = type();
+	  if (result != SYNTAX_OK) return result;
 	  if (token != SEMICOLON) return SYNTAX_ERROR;
-	  token = getNextToken();
-	  if ((n_declaration()) != SYNTAX_OK) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  result = n_declaration();
+	  if (result != SYNTAX_OK) return result;
 	  return SYNTAX_OK;
 	  break;
 
@@ -111,6 +125,7 @@ int n_declaration(){/*<N_DECLARATION>*/
 	
     default:
       return SYNTAX_ERROR;
+	  break;
   }	  
 }
 
@@ -120,40 +135,343 @@ int function(){
 
 int body(){/*<BODY>*/
   
+  int result;
+  
   switch (token){
     /*<BODY> -> BEGIN <ELEMENT> END*/
     case BEGIN:
-	  token = getNextToken();
-	  if ((element()) != SYNTAX_OK) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  result = element();
+	  if (result != SYNTAX_OK) return result;
 	  if (token != END) return SYNTAX_ERROR;
-	  token = getNextToken();
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
 	  return SYNTAX_OK;
-	break;
+	  break;
 	
 	default:
 	  return SYNTAX_ERROR;
+	  break;
   }
 }
 
 int element(){/*<ELEMENT>*/
-  /*<ELEMENT> -> eps*/
-  if (token == END)
-    return SYNTAX_OK;
-  else{
-    /*<ELEMENT> -> <SELECT_ELEMENT> <N_ELEMENT>*/  
-    if ((select_element()) != SYNTAX_OK) return SYNTAX_ERROR;
-    if ((n_element()) != SYNTAX_OK) return SYNTAX_ERROR;
-	return SYNTAX_OK;
-    }	
+ 
+  int result;
+ 
+  switch (token){
+    /*<ELEMENT> -> <SELECT_ELEMENT> <N_ELEMENT>*/
+    case BEGIN:
+	case ID:
+	case WHILE:
+	case IF:
+	  result = select_element();
+	  if (result != SYNTAX_OK) return result;
+	  result = n_element();
+      if (result != SYNTAX_OK) return result;
+	  return SYNTAX_OK;
+	  break;
+	
+	/*<ELEMENT> -> eps*/
+    case END:
+       return SYNTAX_OK;
+       break;
+
+    default:
+      return SYNTAX_ERROR;
+      break;	  
+    } 
 }
 
-int select_element(){/*SELECT_ELEMENT*/
+int select_element(){/*<SELECT_ELEMENT>*/
 
+  int result;
   
-
+  switch (token){
+    /*<SELECT_ELEMENT> -> <BODY>*/
+	case BEGIN:
+	  result = body();
+	  if (result != SYNTAX_OK) return result;
+	  return SYNTAX_OK;
+	  break;
+	  
+	 /*<SELECT_ELEMENT> -> <CALLFUNASS>*/  
+	case ID:
+	  result = callfunass();
+	  if (result != SYNTAX_OK) return result;
+	  return SYNTAX_OK;
+	  break;
+	  
+	 /*<SELECT_ELEMENT> -> <IF>*/  
+	case WHILE:
+	  result = while_condition();
+	  if (result != SYNTAX_OK) return result;
+	  return SYNTAX_OK;
+	  break;
+	
+	 /*<SELECT_ELEMENT> -> <WHILE>*/
+	case IF:
+	  result = if_condition();
+	  if (result != SYNTAX_OK) return result;
+	  return SYNTAX_OK;
+	  break;
+    
+    default:
+      return SYNTAX_ERROR;
+      break;
+    }	  
 }
 
+int n_element(){/*<N_ELEMENT>*/
 
+  int result;
+ 
+  switch (token){
+    /*<N_ELEMENT> -> SEMICOLON <SELECT_ELEMENT> <N_ELEMENT>*/
+    case SEMICOLON:
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  result = select_element();
+	  if (result != SYNTAX_OK) return result;
+	  result = n_element();
+	  if (result != SYNTAX_OK) return result;
+	  return SYNTAX_OK;
+	
+	case END:
+	  return SYNTAX_OK;
+	  break;
+	
+	default:
+	  return SYNTAX_ERROR;
+	  break;
+    }
+}
+
+int callfunass(){/*<CALLFUNASS>*/
+  
+  int result;
+  
+  switch (token){
+    /*<CALLFUNASS> -> ID ASS <CALLORASS>*/
+    case ID:
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  if (token != ASS) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  result = callorass();
+	  if (result != SYNTAX_OK) return result;
+	  return SYNTAX_OK;
+	  break;
+    
+    default:
+      return SYNTAX_ERROR;
+      break;	  
+    }
+}
+
+int callorass(){
+/*
+  int result;
+  bool vyraz;
+  
+  /*Tady bude nejaka funkce, ktera rozhodne zda jde o vyraz, volani funkce nebo o fail*/
+  
+  /*<CALLORASS> -> expression*/
+/*  if (vyraz){*/
+    /*Tady bude zpracovani vyrazu podle precedencni syntakticke analyzy*/
+	/*}*/
+  
+  /*<CALLORASS> -> <ID_FUNCTION> L_BRACKET <VARIABLE> R_BRACKET*/  
+/*  else{
+    result = id_function();
+	if (result != SYNTAX_OK) return result;
+	if (token != L_BRACKET) return SYNTAX_ERROR;
+	if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	result = variable();
+	if (result != SYNTAX_OK) return result;
+	if (token != R_BRACKET) return SYNTAX_ERROR;
+	if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	return SYNTAX_OK;
+	}
+  return SYNTAX_ERROR;	
+*/
+
+  int result;
+  
+  switch (token){
+    /*<CALLORASS> -> EXPRESSION*/
+    case EXPRESSION:
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  return SYNTAX_OK;
+	  break;
+	 
+    /*<CALLORASS> -> <ID_FUNCTION> L_BRACKET <VARIABLE> R_BRACKET*/  
+	case ID:
+    case READLN:
+    case WRITE:
+    case LENGHT:
+    case COPY:
+    case FIND:
+    case SORT:
+	  result = id_function();
+	  if (result != SYNTAX_OK) return result;
+	  if (token != L_BRACKET) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  result = variable();
+	  if (result != SYNTAX_OK) return result;
+	  if (token != R_BRACKET) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  return SYNTAX_OK;
+	  break;
+	  
+	default:
+	  return SYNTAX_ERROR;
+	  break;
+	  
+}
+
+int id_function(){
+  
+  switch (token){
+    /*<FUNCTION_ID> -> ID, analogicky pro dalsi identifikatory*/
+	case ID:
+    case READLN:
+    case WRITE:
+    case LENGHT:
+    case COPY:
+    case FIND:
+    case SORT:
+      if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  return SYNTAX_OK;
+	  break;
+    
+    default:
+      return SYNTAX_ERROR;
+      break;	  
+    }
+}
+
+int variable(){
+
+  int result;
+  
+  switch (token){
+    /*<VARIABLE> -> <VALUE> <N_VARIABLE>*/
+	case INTEGER:
+	case STRING:
+	case DES_INT:
+	case DES_EXP:
+	case DES_EXP_NEG:
+	case EXP:
+	case EXP_NEG:
+	  result = value();
+	  if (result != SYNTAX_OK) return result;
+	  result = n_variable();
+	  if (result != SYNTAX_OK) return result;
+	  return SYNTAX_OK;
+	  break;
+	
+	case R_BRACKET:
+	  return SYNTAX_OK;
+	  break;
+	  
+	default:
+	   return SYNTAX_ERROR;
+	   break;  
+	}   
+}
+
+int value(){
+  
+  switch (token){
+    /*<VALUE> -> INTEGER, analogicky pro zbytek*/
+	case INTEGER:
+	case STRING:
+	case DES_INT:
+	case DES_EXP:
+	case DES_EXP_NEG:
+	case EXP:
+	case EXP_NEG:
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  return SYNTAX_OK;
+      break;
+	  
+	default:
+	  return SYNTAX_ERROR;
+	  break;
+	}
+}
+
+int n_variable(){
+  
+  int result;
+  
+  switch (token){
+    case COMMA:
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  result = value();
+	  if (result != SYNTAX_OK) return result;
+	  result = n_variable();
+	  if (result != SYNTAX_OK) return result;
+	  return SYNTAX_OK;
+	  break;
+    
+    case R_BRACKET:
+      return SYNTAX_OK;
+      break;
+
+    default:
+      return SYNTAX_ERROR;	
+	  break;
+    }
+}
+
+int while_condition(){/*<WHILE_CONDITION>*/
+
+  int result;
+  
+  switch (token){
+    /*<WHILE_CONDITION> -> WHILE EXPRESSION DO <BODY>*/
+    case WHILE:
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  if (token != EXPRESSION) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  if (token != DO) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  result = body();
+	  if (result != SYNTAX_OK) return result;
+	  return SYNTAX_OK;
+	  break;
+	  
+	default:
+	  return SYNTAX_ERROR;
+	  break;
+    }
+}
+
+int if_condition(){
+
+  int result;
+  
+  switch (token){
+    /*<IF_CONDITION> -> IF EXPRESSION THEN <BODY> ELSE <BODY>*/
+	case IF:
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  if (token != EXPRESSION) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  if (token != THEN) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  result = body();
+	  if (result != SYNTAX_OK) return result;
+	  if (token != ELSE) return SYNTAX_ERROR;
+	  if ((token = getNextToken(&attr)) == LEX_ERROR) return LEX_ERROR;
+	  result = body();
+	  if (result != SYNTAX_OK) return result;
+	  return SYNTAX_OK;
+	  break;
+	
+	default:
+	  return SYNTAX_ERROR;
+	  break;
+	}
+}
 
 
 
