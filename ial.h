33fca6155481c7e75118f79ddb512ae168cab523
Filip Denk <filip.denk@gmail.com>
2014-12-04 16:15:39 +0100

    ial.h

--- ial.h
+++ ial.h
@@ -76,4 +76,10 @@ struct htab_item* search_var(char *name, struct symbol_table* s_table, int* erro
 void symbol_table_free(struct symbol_table* s_table);
 int funcs_defined(struct symbol_table* s_table); //overi, zda vsechny deklarovane fce v tabulce byly i definovane. pokud ne -> return SEM_ERROR
 
-					
+
+int length(char *s);
+int copy(char *s, string *dest, int i, int n, int s_len);
+int find(char *s, char *search, int s_len, int search_len);
+int sort(string *s, string *dest);
+void sort_main(char *A, int left, int right);
+void partition(char *A, int left, int right, int *i, int *j);
