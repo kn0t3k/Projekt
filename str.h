08002703a32d9951c309806d72cb3c4a39e1db91
tomasjuri <tomasjuri@gmail.com>
2014-12-05 22:40:30 +0100

    Update str.h

--- str.h
+++ str.h
@@ -1,3 +1,15 @@
+/*--------------------------------------
+| Projekt: IFJ14						
+| Soubor: str.h						
+| Autori: Denk Filip (xdenkf00)		
+|	  Jurica Tomas (xjuric22)		
+|	  Knotek Martin (xknote11)	
+|	  Kohut Jan (xkohut08)		
+|	  Piskula David (xpisku02)	
+| Prekladac: GCC 4.8.2-7				
+| Datum:   5.12.2014					
+|--------------------------------------*/
+
 //hlavickovy soubor pro praci s nekonecne dlouhymi retezci
 
 typedef struct
