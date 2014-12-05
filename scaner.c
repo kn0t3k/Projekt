213c87f59fcf257ef22d10952677716c42f87998
tomasjuri <tomasjuri@gmail.com>
2014-12-05 22:39:32 +0100

    Update scaner.c

--- scaner.c
+++ scaner.c
@@ -1,4 +1,14 @@
-    //Martin Knotek xknote11
+/*--------------------------------------
+| Projekt: IFJ14						
+| Soubor: scaner.c						
+| Autori: Denk Filip (xdenkf00)		
+|	  Jurica Tomas (xjuric22)		
+|	  Knotek Martin (xknote11)	
+|	  Kohut Jan (xkohut08)		
+|	  Piskula David (xpisku02)	
+| Prekladac: GCC 4.8.2-7				
+| Datum:   5.12.2014					
+|--------------------------------------*/
 
     // lexikalni analyzator
     #include <stdio.h>
