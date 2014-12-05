6cb479280a03e3cd884b4424102b1db0da4dcce2
tomasjuri <tomasjuri@gmail.com>
2014-12-05 22:30:26 +0100

    Update err.h

--- err.h
+++ err.h
@@ -1,11 +1,11 @@
 /*--------------------------------------
 | Projekt: IFJ14						
-| Soubor: main.c						
+| Soubor: err.h						
 | Autori:	Denk Filip (xdenkf00)		
-|			Jurica Tomas (xjuric22)		
-|			Knotek Martin (xknote11)	
-|			Kohut Jan (xkohut08)		
-|			Piskula David (xpisku02)	
+|	Jurica Tomas (xjuric22)		
+|	Knotek Martin (xknote11)	
+|	Kohut Jan (xkohut08)		
+|	Piskula David (xpisku02)	
 | Prekladac: GCC 4.8.2-7				
 | Datum:   5.12.2014					
 |--------------------------------------*/
