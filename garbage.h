65343b443fa42c5794ec73a9f1eed7cf8c9401a1
tomasjuri <tomasjuri@gmail.com>
2014-12-09 00:02:55 +0100

    Create garbage.h

---
+++ garbage.h
@@ -0,0 +1,14 @@
+
+
+typedef struct garbage_item{
+	void* ptr;
+	struct garbage_item* next;
+	} garbage_item;
+
+typedef struct garbage_list{
+	struct garbage_item* first;
+	} garbage_list;	
+	
+struct garbage_list* garbage_init();	
+int garbage_add(struct garbage_list* Glist, void* Gptr);
+void garbage_free(struct garbage_list* Glist);
